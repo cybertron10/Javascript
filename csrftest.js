a6360bd3bd86d3cf4fab158e36b5c6ccfa9ac62e
cybertron10 <56225601+cybertron10@users.noreply.github.com>
2020-08-22 19:10:58 -0400

    Update csrftest.js

--- csrftest.js
+++ csrftest.js
@@ -35,15 +35,13 @@ if (req.readyState == 4 && req.status == 200 )
    var reponse_obj = JSON.parse(req.responseText);
    for (i = 0; i < reponse_obj.rows.length; i++) {
    console.log(reponse_obj.rows[i].id);
-      req2.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=1&msgid="+reponse_obj.rows[i].id, true);
-      req2.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      req2.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=1&msgid="+reponse_obj.rows[i].id+"&nact=1, true);
       req2.send();
        }
    }
  }; 
 
-req.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messageJson.jsp?uid=9135&type=0&tab=3&_search=false&rows=20&page=1&sidx=&sord=desc");
-req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+req.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messageJson.jsp?uid=9135&type=0&tab=3&_search=false&rows=20&page=1&sidx=&sord=desc");
 req.send();
 /*
 req5.onreadystatechange = function()
