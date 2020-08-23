1a84bb47190207b9f96a5aec1eb54dd5d2c14b9a
cybertron10 <56225601+cybertron10@users.noreply.github.com>
2020-08-22 20:57:44 -0400

    Update csrftest.js

--- csrftest.js
+++ csrftest.js
@@ -26,5 +26,5 @@
    }
 };
 
-     req.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messageJson.jsp?uid=9135&type=0&tab=4&rows=20&page=1");
+     req.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messageJson.jsp?uid=9135&type=0&tab=3&rows=50&page=1");
      req.send();
