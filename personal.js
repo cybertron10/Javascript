4bbd07deddb6ee97cef79640117c9fdeba14121d
cybertron10 <56225601+cybertron10@users.noreply.github.com>
2020-08-23 10:13:13 -0400

    Update personal.js

--- personal.js
+++ personal.js
@@ -3,9 +3,8 @@ var req = new XMLHttpRequest();
 req.onreadystatechange = function () {
 if (req.readyState == 4 && req.status == 200 )
  {
- var htmlPage = req.responseXML;
  
- var phoneno = htmlPage.getElementById("per_CellPhone").value;
+ var phoneno = document.getElementById("per_CellPhone").value;
  
  var email = document.getElementById("per_email").value;
  
@@ -15,5 +14,4 @@ if (req.readyState == 4 && req.status == 200 )
 };
 
 req.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/personalInfo_New.jsp?mainNav=Account&pgId=personalInfo_New&act=0", true);
-req.responseType = "document";
 req.send();
