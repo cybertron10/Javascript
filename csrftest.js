383fb56917595c513be7709dedb82ba445da67e7
cybertron10 <56225601+cybertron10@users.noreply.github.com>
2020-08-22 20:03:17 -0400

    Update csrftest.js

--- csrftest.js
+++ csrftest.js
@@ -1,66 +1,28 @@
-var req = new XMLHttpRequest();
-var req2 = new XMLHttpRequest();
-var req3 = new XMLHttpRequest();
-var req4 = new XMLHttpRequest();
-var req5 = new XMLHttpRequest();
-var req6 = new XMLHttpRequest();
-var i = '';
-var row = '';
-/*req.onreadystatechange = function()
-{
-if (req.readyState == 4 && req.status == 200 )
-  {
- console.log("Deleting Inbox");
- reponse_obj = JSON.parse(req.responseText);
- for (i = 0; i < reponse_obj.rows.length; i++) {
-    var row1 = reponse_obj.rows[i];
-    console.log(row1.id);
-    req2.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=0&msgid="+row1.id+"&nact=1&jqg_jqGridDelete_"+row1.id+"=on", true);
-    req2.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    req2.send();
-   }
-  }
- };  
+   var req = new XMLHttpRequest();
 
-
-req.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messageJson.jsp?uid=9135&type=0&tab=2&_search=false&rows=20&page=1&sidx=&sord=desc");
-req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-req.send();
-*/
-for (i=0; i < 20; i++){
-req.onreadystatechange = function()
-{
-if (req.readyState == 4 && req.status == 200 )
+   req.onreadystatechange = function()
    {
-   console.log("Deleting SentBox");
-   var reponse_obj = JSON.parse(req.responseText);
-      req2.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=1&msgid="+reponse_obj.rows[0].id+"&nact=1", true);
-      req2.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-      req2.send();
-    }
- }; 
-
-req.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messageJson.jsp?uid=9135&type=0&tab=3&_search=false&rows=20&page=1&sidx=&sord=desc");
-req.send();
-}
-/*
-req5.onreadystatechange = function()
-{
-if (req5.readyState == 4 && req5.status == 200 )
-  {
- console.log("Deleting DeleteBox");
- reponse_obj = JSON.parse(req5.responseText);
- for (var i = 0; i < reponse_obj.rows.length; i++) {
-    var row3 = reponse_obj.rows[i];
-    console.log(row3.id);
-    req6.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=2&msgid="+row3.id+"&nact=1&jqg_jqGridDelete_"+row3.id+"=on", true);
-    req6.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    req6.send();
+     if (req.readyState == 4 && req.status == 200 )
+   {
+     reponse_obj = JSON.parse(req.responseText);
+     for (var i = 0; i < reponse_obj.rows.length; i++) {
+     var row = reponse_obj.rows[i];
+     console.log(row.id);
+     var req2 = new XMLHttpRequest();
+     req2.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=0&msgid="+row.id+"&nact=1", true);
+     req2.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+     req2.send();
+     var req3 = new XMLHttpRequest();
+     req3.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=1&msgid="+row.id+"&nact=1", true);
+     req3.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+     req3.send();
+     var req4 = new XMLHttpRequest();
+     req4.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messages.jsp?box=2&msgid="+row.id+"&nact=1", true);
+     req4.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+     req4.send();
+     }
    }
-  }
- }; 
+};
 
-/*req5.open("POST", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messageJson.jsp?uid=9135&type=0&tab=4&_search=false&rows=20&page=1&sidx=&sord=desc");
-req5.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-req5.send();
-*/
+     req.open("GET", "https://appsecpatientportal.eclinicalweb.com/portal_trunk/jsp/jspnew/messageJson.jsp?uid=9135&type=0&tab=3&rows=20&page=1");
+     req.send();
